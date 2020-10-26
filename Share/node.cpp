34b0db85c76618d34f443da29ae0d984ee901717
BernardoM08 <71307281+BernardoM08@users.noreply.github.com>
2020-10-26 00:19:03 -0700

    Add files via upload

---
+++ Share/node.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+
+#include "node.h"
+
+using namespace std;
+
+Node::Node(Student* newstudent)
+{
+  student = newstudent;
+  next = NULL;
+}
+
+Node::~Node()
+{
+  delete &student;
+  next = NULL;
+}
+
+//setters
+void Node::setStudent(Student* newStudent)
+{
+  student = newStudent;
+}
+
+void Node::setNext(Node* newNext)
+{
+  next = newNext;
+}
+
+//getters
+Student* Node::getStudent()
+{
+  return student;
+}
+
+Node* Node::getNext()
+{
+  return next;
+}
+
